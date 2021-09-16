0b6e227eaaa2ddb0dd4a0158f28f0d2603b4c840
gagamel6 <iamyuk6@gmail.com>
2021-09-16 17:14:23 +0900

    웹크롤링

---
+++ web1.py
@@ -0,0 +1,24 @@
+# web1.py
+from bs4 import BeautifulSoup
+
+#웹페이지를 로딩(유니코드)
+#메소드를 연속으로 호출(메서드 체인)
+#페이지는 문자열 변수
+page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
+#검색을 잘하는 스프객체 생성(html태그 파싱)
+soup = BeautifulSoup(page, "html.parser")
+#print( soup.prettify() )
+#<p>태그 몽땅 가져와~~
+#print( soup.find_all("p") )
+#<a>태그 몽땅 가져와~~
+#print( soup.find_all("a") )
+#첫번째 <p>태그만 가져오기
+#print( soup.find("p") )
+#특정 속성을 검색: <p class='outer'>컨텐츠</p>
+#print( soup.find_all("p", class_="outer-text") )
+
+#문자열만 가죠오기<p>교육센터</p>
+for tag in soup.find_all("p"):
+    #태그 안쪽에 문자열: .text 속성
+    print( tag.text.strip() )
+
---
+++ 클리앙중고장터검색.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#우리 웹사이트는 웹봇을 금지합니다. ~~
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+#0번페이지에서 9번까지 10번 * 30 ==> 300개
+for n in range(0,10):
+        #클리앙의 중고장터 주소 
+        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        #웹브라우져 헤더 추가 
+        print(data)
+        req = urllib.request.Request(data, \
+                                    headers = hdr)
+        data = urllib.request.urlopen(req).read()
+        page = data.decode('utf-8', 'ignore')
+        soup = BeautifulSoup(page, 'html.parser')
+
+	# <span data-role="list-title-text" title="맥북에어 M1 고급형">
+	# 	맥북에어 M1 고급형
+	# </span>
+
+        list = soup.find_all('span', attrs={'data-role':'list_title-text'})
+
+        for item in list:
+                try:
+                        title = item.text
+                        #print( title.strip() ) 
+                        if (re.search('맥북', title)):
+                                print(title.strip())
+                except:
+                        pass
+        
+
